cf51721031cc1ff2c626d58c025c80516aa841bf
ashram7 <ashram7@hanmail.net>
2021-11-02 12:39:13 +0900

    main folder added

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>H스쿨 PHP 프로그래밍</title>
-	<link rel="stylesheet" type="text/css" href="./css/common.css">
-	<link rel="stylesheet" type="text/css" href="./css/main.css">
+	<link rel="stylesheet" type="text/css" href="./main/css/common.css">
+	<link rel="stylesheet" type="text/css" href="./main/css/main.css">
 </head>
 <body>
 	<header>
-		<?php include "./header.php";?>
+		<?php include "./main/header.php";?>
 	</header>
 	<section>
-		<?php include "./include/main.php";?>
+		<?php include "./main/main.php";?>
 	</section> 
 	<footer>
-		<?php include "./include/footer.php";?>
+		<?php include "./main/footer.php";?>
 	</footer>	
 </body>
 </html>
--- main/header.php
+++ main/header.php
@@ -1,5 +1,5 @@
 <?php
-    include "include/define.php";
+    include "../include/define.php";
 
     session_start(); 
     if (isset($_SESSION["userid"])) $userid = $_SESSION["userid"]; 
@@ -22,18 +22,18 @@
 <?php 
     if(!$userid) { 
 ?>
-                <li><a href="./login/member_form.php">회원 가입(11Ch)</a> </li> 
+                <li><a href="../login/member_form.php">회원 가입(11Ch)</a> </li> 
                 <li> | </li> 
-                <li><a href="./login/login_form.php">로그인(12Ch)</a></li> 
+                <li><a href="../login/login_form.php">로그인(12Ch)</a></li> 
 <?php 
     } else { 
         $logged = $username."(".$userid.")님[Level:".$userlevel.", Point:".$userpoint."]"; 
 ?>  
                 <li><?=$logged?> </li> 
                 <li> | </li> 
-                <li><a href="./login/logout.php">로그아웃(12Ch)</a> </li> 
+                <li><a href="../login/logout.php">로그아웃(12Ch)</a> </li> 
                 <li> | </li> 
-                <li><a href="./login/member_modify_form.php">정보 수정(12Ch)</a></li>
+                <li><a href="../login/member_modify_form.php">정보 수정(12Ch)</a></li>
 <?php 
     } 
 ?>  
@@ -41,7 +41,7 @@
     if($userlevel==1) {
 ?>
                 <li> | </li> 
-                <li><a href="./admin/admin.php">관리자 모드(15Ch)</a></li> 
+                <li><a href="../admin/admin.php">관리자 모드(15Ch)</a></li> 
 <?php
     }
 ?> 
@@ -50,8 +50,8 @@
         <div id="menu_bar"> 
             <ul>   
                 <li><a href="./index.php">HOME</a></li> 
-                <li><a href="./message/message_form.php">쪽지 만들기(13Ch)</a></li> 
-                <li><a href="./board/board_form.php">게시판 만들기(14Ch)</a></li> 
+                <li><a href="../message/message_form.php">쪽지 만들기(13Ch)</a></li> 
+                <li><a href="../board/board_form.php">게시판 만들기(14Ch)</a></li> 
                 <li><a href="./index.php">사이트 완성하기(15Ch)</a></li> 
             </ul> 
         </div>                              
